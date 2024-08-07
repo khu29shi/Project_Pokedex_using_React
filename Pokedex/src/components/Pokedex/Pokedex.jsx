cdcf135a1e2b4eb51adc2d6b13edd5a96e3b6405
Khushboo <khushbookarsh1112@gmail.com>
2024-08-07 23:04:07 +0530

    added some changes

--- Pokedex/src/components/Pokedex/Pokedex.jsx
+++ Pokedex/src/components/Pokedex/Pokedex.jsx
@@ -1,11 +1,13 @@
 import Search from "../Search/Search"
 import "./Pokedex.css"
+import PokemonList from "../PokemonList/PokemonList"
 
 function Pokedex() {
   return (
     <div className="pokedex-wrapper">
       <h1 id="pokedex-heading">Pokedex</h1>
       <Search/>
+      <PokemonList/>
     </div>
   )
 }

d807962af56c57e07107786f656c46a3532aba49
Khushboo <khushbookarsh1112@gmail.com>
2024-08-07 14:01:55 +0530

    added some changes

--- Pokedex/src/components/Pokedex/Pokedex.jsx
+++ Pokedex/src/components/Pokedex/Pokedex.jsx
@@ -1,17 +1,14 @@
 import Search from "../Search/Search"
-import './Pokedex.css'
-import PokemonList from "./PokemonList/PokemonList"
-import { useState } from "react";
+import "./Pokedex.css"
 
 function Pokedex() {
-    const [ search, setSearch ] = useState("");
   return (
     <div className="pokedex-wrapper">
-      <Search search={search} setSearch={setSearch} />
-      <PokemonList search={search} />
+      <h1>pokedex</h1>
+      <Search/>
     </div>
   )
 }
 
 export default Pokedex
-
+ 

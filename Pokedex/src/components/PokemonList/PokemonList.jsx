cd7a7e188aa52791f04a8041660eb62a1c93afdb
Khushboo <khushbookarsh1112@gmail.com>
2024-08-08 01:25:27 +0530

    It's working on the browser we see the fetched data result

--- Pokedex/src/components/PokemonList/PokemonList.jsx
+++ Pokedex/src/components/PokemonList/PokemonList.jsx
@@ -1,30 +1,28 @@
-import React, { useEffect } from 'react'
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import "./PokemonList.css"
+import Pokemon from "../Pokemon/Pokemon"
 
 const PokemonList = () => {
 
     const [pokemonList, setPokemonList] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     async function downloadPokemons() {
         const response =await axios.get("https://pokeapi.co/api/v2/pokemon/");
             console.log(response.data);
             console.log(response);
-
             const pokemonResults = response.data.results;
             console.log(pokemonResults);
-
             const pokemonResultPromise = pokemonResults.map((pokemon) => axios.get(pokemon.url))
             console.log(pokemonResultPromise);
-
             const pokemonData = await axios.all(pokemonResultPromise)
             console.log(pokemonData);
-
             const result = pokemonData.map((pokeData) => {
                 const pokemon = pokeData.data;
                 console.log(pokemon);
                 return {
+                    id: pokemon.id,
                     name: pokemon.name,
                     image: pokemon.sprites.other.dream_world.front_default,
                     types: pokemon.types
@@ -41,9 +39,8 @@ const PokemonList = () => {
     }, [])
   return (
     <div className="pokemon-list-wrapper">
-        Pokemon List
         <div>Pokemon List</div>
-        {(isLoading) ? 'Loading......' : 'Data downloaded'}
+        {(isLoading) ? 'Loading......' : pokemonList.map((p) => <Pokemon name = {p.name} image = {p.image} key = {p.id} />)}
     </div>
   )
 }

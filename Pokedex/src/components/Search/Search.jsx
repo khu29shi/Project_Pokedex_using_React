43844cff00451ca5ffa31f3b2390850925891e6d
Khushboo <khushbookarsh1112@gmail.com>
2024-08-07 14:13:18 +0530

    designed search bar

---
+++ Pokedex/src/components/Search/Search.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Search.css"
+
+function Search() {
+  return (
+    <div className="search-wrapper">
+      <input
+        id="pokemon-name-search"
+        type="text"
+        placeholder='pokemon name......'
+       />
+    </div>
+  )
+}
+
+export default Search
